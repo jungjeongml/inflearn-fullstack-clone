f192d0860ba460bbd936405fb75bc551a46e73fd
jungmin <justin930522@gmail.com>
2026-05-13 11:42:15 +0900

    [BE]강의 상세페이지 API 구현

--- backend/src/courses/courses.controller.ts
+++ backend/src/courses/courses.controller.ts
@@ -17,15 +17,16 @@ import {
   ApiQuery,
   ApiTags,
 } from '@nestjs/swagger';
+import { Prisma } from '@prisma/client';
 import type { Request } from 'express';
+import { Course as CourseEntity } from 'src/_gen/prisma-class/course';
 import { AccessTokenGuard } from 'src/auth/guards/access-token.guard';
 import { CoursesService } from './courses.service';
+import { CourseDetailDto } from './dto/course-detail.dto';
 import { CreateCourseDto } from './dto/create-course.dto';
-import { UpdateCourseDto } from './dto/update-course.dto';
-import { Prisma } from '@prisma/client';
-import { Course as CourseEntity } from 'src/_gen/prisma-class/course';
-import { SearchCourseResponseDto } from './dto/search-response-dto';
 import { SearchCourseDto } from './dto/search-course.dto';
+import { SearchCourseResponseDto } from './dto/search-response-dto';
+import { UpdateCourseDto } from './dto/update-course.dto';
 
 @ApiTags('코스')
 @Controller('courses')
@@ -90,59 +91,13 @@ export class CoursesController {
   }
 
   @Get(':id')
-  @ApiQuery({
-    name: 'include',
-    required: false,
-    description: 'sections, lectures, courseReviews 등 포함할 관계 지정',
-  })
   @ApiOkResponse({
     description: '코스 상세 정보',
-    type: CourseEntity,
+    type: CourseDetailDto,
   })
   // findOne부분 수정
-  findOne(
-    @Param('id', ParseUUIDPipe) id: string,
-    @Query('include') include?: string,
-  ) {
-    const includeArray = include
-      ? include.split(',').map((item) => item.trim())
-      : undefined;
-
-    let includeObject: Prisma.CourseInclude;
-
-    if (
-      includeArray?.includes('sections') &&
-      includeArray?.includes('lectures')
-    ) {
-      const otherInclude = includeArray.filter(
-        (item) => !['sections', 'lectures'].includes(item),
-      );
-      includeObject = {
-        sections: {
-          include: {
-            lectures: {
-              orderBy: {
-                order: 'asc',
-              },
-            },
-          },
-          orderBy: {
-            order: 'asc',
-          },
-        },
-        ...otherInclude.map((item) => ({
-          [item]: true,
-        })),
-      };
-    } else {
-      includeObject = {
-        ...includeArray?.map((item) => ({
-          [item]: true,
-        })),
-      } as Prisma.CourseInclude;
-    }
-
-    return this.coursesService.findOne(id, includeObject);
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.coursesService.findOne(id);
   }
 
   @Patch(':id')
--- backend/src/prisma/prisma.service.ts
+++ backend/src/prisma/prisma.service.ts
@@ -3,6 +3,16 @@ import { PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
+  constructor() {
+    super({
+      omit: {
+        user: {
+          hashedPassword: true,
+        },
+      },
+    });
+  }
+
   async onModuleInit() {
     await this.$connect(); //모듈이 초기화될 때 데이터베이스와 연결
   }
