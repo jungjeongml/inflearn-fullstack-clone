890a129fea658eff89bf60c956a252125b3afe85
jungmin <justin930522@gmail.com>
2026-05-25 11:00:39 +0900

    [BE] 수강평 목록,작성API 구현

---
+++ backend/src/courses/dto/create-review.dto.ts
@@ -0,0 +1,21 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { IsInt, IsNotEmpty, IsString, Max, Min } from 'class-validator';
+
+export class CreateReviewDto {
+  @ApiProperty({
+    description: '수강평 내용',
+    example: '정말 유익한 강의였습니댜.',
+  })
+  @IsString()
+  @IsNotEmpty()
+  content: string;
+
+  @ApiProperty({
+    description: '평정 (1~5)',
+    example: 5,
+  })
+  @IsInt()
+  @Min(1)
+  @Max(5)
+  rating: number;
+}
